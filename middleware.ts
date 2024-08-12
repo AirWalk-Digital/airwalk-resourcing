79ea2cfcca68baa4ce943912d4bb3654f306e3c8
Andy Payne <andy.payne@airwalkconsulting.com>
2024-08-12 16:34:16 +0100

    feat: remove logging statements

--- middleware.ts
+++ middleware.ts
@@ -2,15 +2,7 @@ import type { NextRequest } from 'next/server';
 import { NextResponse } from 'next/server';
 
 export function middleware(request: NextRequest) {
-  console.log('hostname');
-  console.log(process.env.HOSTNAME);
-
-  console.log('call1');
-  console.log(request.url);
-  console.log(request.nextUrl.pathname);
-  console.log('');
-
-  let response;
+  let response = NextResponse.next();
 
   if (request.nextUrl.pathname.startsWith('/api/resourcing')) {
     const newUrl = new URL(request.url);
@@ -21,24 +13,8 @@ export function middleware(request: NextRequest) {
 
     response = NextResponse.rewrite(newUrl);
 
-    console.log('call3');
-    console.log(response);
-    console.log('');
-
     return response;
   }
-
-  console.log('call2');
-
-  console.log(request.url);
-  console.log(request.nextUrl.pathname);
-  console.log();
-
-  response = NextResponse.next();
-
-  console.log('call4');
-  console.log(response);
-  console.log();
 
   return response;
 }
